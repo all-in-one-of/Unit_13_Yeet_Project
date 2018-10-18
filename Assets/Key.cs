4c7b7bb5ccf197749c5ffcbfdd92f55621eb2631
Richard <44157733+BanksR@users.noreply.github.com>
2018-10-18 16:16:03 +0100

    Add files via upload

---
+++ Assets/Key.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public int keyID;
+    public string keyName;
+    public KeyChain keyChain;
+
+	private void OnTriggerEnter(Collider other)
+	{
+        if(other.gameObject.CompareTag("Player"))
+        {
+
+
+            // Add this Key to our KeyChain
+            // Play a pick-up SFX
+            keyChain.AddKey(this);
+
+            this.gameObject.SetActive(false);
+        }
+	}
+}
---
+++ Assets/KeyChain.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyChain : MonoBehaviour 
+{
+    //Initialise a new List of Key called keys
+    public List<Key> keys = new List<Key>();
+
+
+    //This public function will be called from other classes
+    public void AddKey (Key keyToAdd)
+    {
+        keys.Add(keyToAdd);
+    }
+
+    //This function removes a key if necassary
+    public void RemoveKey (Key keyToRemove)
+    {
+        keys.Remove(keyToRemove);
+    }
+
+
+
+
+
+}

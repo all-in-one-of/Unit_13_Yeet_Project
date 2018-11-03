e17e73ec868813e5513ffeae5c19713f6a64e1f4
Richard <r.banks@burnley.ac.uk>
2018-11-03 22:38:11 +0000

    Added a swinging Trap

---
+++ Assets/SwingTrapController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingTrapController : MonoBehaviour
+{
+    private HingeJoint hingeControl;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        hingeControl = GetComponentInChildren<HingeJoint>();
+        hingeControl.useMotor = true;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            hingeControl.useMotor = !hingeControl.useMotor;
+        }
+	}
+}

62cd18d029733ffe96b50a08df2effc401a45360
Richard <r.banks@burnley.ac.uk>
2018-11-19 15:50:12 +0000

    Started work on a simple dialogManager

---
+++ Assets/DialogManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+
+	public static DialogManager Instance;
+	public Text uiText;
+	public Text namePlate;
+	public DialogText dialogText;
+
+	//public bool hasTextToDisplay = true;
+
+	private Animator _anims;
+	
+	
+	// Use this for initialization
+	void Awake () 
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+
+		else
+		{
+			Destroy(this.gameObject);
+		}
+
+		_anims = GetComponent<Animator>();
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.anyKeyDown && dialogText != null)
+		{
+			DisplayText(dialogText.DQ());
+		}
+
+		if (dialogText != null)
+		{
+			_anims.SetBool("PopBool", dialogText.hasTextToDisplay);
+			namePlate.text = dialogText.DialogGiversName;
+		}
+
+		
+		
+	
+		
+	}
+
+	public void DisplayText(string textToDisplay)
+	{
+		uiText.text = textToDisplay;
+	}
+
+	public void TriggerAnim()
+	{
+		_anims.SetTrigger("Pop");
+	}
+
+
+
+}
---
+++ Assets/DialogText.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogText : MonoBehaviour
+{
+
+	[TextArea(10, 10)]
+	public string DialogToGive;
+	private string[] dialogArray;
+	public Queue<string> DialogQ;
+
+	public string DialogGiversName = "";
+
+	public bool hasTextToDisplay = true;
+
+	//public DialogManager dialogManager;
+	
+	// Use this for initialization
+	void Awake ()
+	{
+		
+		ReQueue();
+	}
+	
+	
+	
+
+	public string DQ()
+	{
+
+		if (DialogQ.Count != 0 && hasTextToDisplay)
+		{
+			string t = DialogQ.Dequeue();
+			return t;
+			//DialogManager.Instance.DisplayText(qText);
+
+		}
+
+		hasTextToDisplay = false;
+		return "...";
+
+
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			DialogManager.Instance.dialogText = this;
+			DialogManager.Instance.TriggerAnim();
+			ReQueue();
+		}
+	}
+
+	private void ReQueue()
+	{
+		hasTextToDisplay = true;
+		DialogQ = new Queue<string>();
+		dialogArray = (DialogToGive.Split('\n'));
+		foreach (var line in dialogArray)
+		{
+			DialogQ.Enqueue(line);
+		}
+	}
+}
